8af757708d5a5c5581e15b2acaeca0c251ba79ef
Trey Stafford <trey.stafford@nsidc.org>
2020-12-02 08:36:01 -0700

    Use jupyter lab instead of notebook

--- start_notebook.sh
+++ start_notebook.sh
@@ -3,4 +3,4 @@
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 source activate smap
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --config=$THISDIR/jupyter_notebook_config.py --notebook-dir=$THISDIR/notebooks/
+jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --config=$THISDIR/jupyter_notebook_config.py --notebook-dir=$THISDIR/notebooks/
